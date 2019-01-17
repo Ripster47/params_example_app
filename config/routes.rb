e405f36ba446feeb6a5a483a7cd32b41365e6178
Ripster47 <mpolinski12@gmail.com>
2019-01-17 11:58:36 -0600

    body params

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     get '/string_params_example' => 'params_examples#string_example'
     get 'url_segment_example/:wildcard' => 'params_examples#url_example'
                             #:wildcard and :timmy are wildcards (no colon in browser)
+    post '/body_params_example' => 'params_examples#body_params_example'
   end
 end
